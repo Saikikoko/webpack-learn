942e5347e3e057e9ef1d5cbc77ede2b015824a16
saiki <729897124@qq.com>
2020-08-11 10:44:39 +0800

    entry&&output&&mode&&loader

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+console.log('111')
+// import image from './21135012_p0.jpg'
+
+const ele = document.createElement('div')
+ele.innerHTML = 'hello';
+document.body.appendChild(ele)
+
+// const img = document.createElement('img')
+// img.src = image
+// // img.src = './21135012_p0.jpg'
+// img.onload = function() {
+//   document.body.appendChild(img)
+// }
